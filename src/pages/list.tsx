1c45695af4dacdcc01e65a99f29e8c9fe2483b9a
vuong <nguyenquangvuong112@gmail.com>
2026-03-20 15:06:27 +0700

    comit

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -56,39 +56,39 @@ const StoriesList  = () => {
             align: "center"
         },
         {
-            title: "Title",
+            title: "Tên truyện ",
             dataIndex: "title",
             align: "center"
         },
         {
-            title: "Auhtor",
+            title: "Tác giả",
             dataIndex: "author",
             align: "center"
         },
         {
-            title: "Image",
+            title: "Ảnh",
             dataIndex: "image",
             align: "center",
             render: (image: string) => image ? <img src={image} alt="img cua stories" style={{ width: "90px", height: "90px", objectFit: "cover" }}></img> : "khong co anh"
         },
         {
-            title: "Description",
+            title: "Mô tả",
             dataIndex: "description",
             align: "center"
         },
         {
-            title: "Date",
+            title: "Ngày",
             dataIndex: "date",
             align: "center"
         },
         {
-            title: "Status",
+            title: "Trạng thái",
             dataIndex: "isActive",
             render: (active: boolean) => (<span style={{ border: active ? "3px solid #00f500" : "3px solid red", borderRadius: "50%", padding: "5px 16px" }}>{active ? "Còn" : "Hết"}</span>),
             align: "center",
         },
         {
-            title: "Action",
+            title: "Hành động",
             align: "center",
             render: (_: any, item: Stories) => (
                 <div style={{ display: "flex", justifyContent: "center", gap: 10 }}>
